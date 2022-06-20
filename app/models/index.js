c3e54b090058abbe00656bebf6dea37452072b32
Nicola Pompini <nicola.pompini@gmail.com>
2022-06-21 00:35:07 +0200

    Updated DB configuration

--- app/models/index.js
+++ app/models/index.js
@@ -1,23 +1,37 @@
-const config = process.env.NODE_ENV === 'production'
-    ? require("../config/db.config.js")
-    : require("../config/db.dev-config.js");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-    config.DB,
-    config.USER,
-    config.PASSWORD,
-    {
-        host: config.HOST,
-        dialect: config.dialect,
-        operatorAliases: false,
-        pool: {
-            max: config.pool.max,
-            min: config.pool.min,
-            acquire: config.pool.acquire,
-            idle: config.pool.idle,
+var sequelize;
+// Configure database
+if (process.env.NODE_ENV === 'production') {
+    sequelize = new Sequelize(
+        process.env.DATABASE_URL,
+        {
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false,
+                }
+            }
         }
-    }
-);
+    )
+} else {
+    const config = require("../config/db.config.js");
+    sequelize = new Sequelize(
+        config.DB,
+        config.USER,
+        config.PASSWORD,
+        {
+            host: config.HOST,
+            dialect: config.dialect,
+            operatorAliases: false,
+            pool: {
+                max: config.pool.max,
+                min: config.pool.min,
+                acquire: config.pool.acquire,
+                idle: config.pool.idle,
+            }
+        }
+    );
+}
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
